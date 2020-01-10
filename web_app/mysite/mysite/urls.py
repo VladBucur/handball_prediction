383f4094944d2f8d53eb052e3aeb7085132694af
VladBucur <vladmihai97@gmail.com>
2020-01-10 21:23:43 +0000

    created home page and player search result page

--- web_app/mysite/mysite/urls.py
+++ web_app/mysite/mysite/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path
 
 from home.views import homePage
+from playerSearch.views import searchView
 
 urlpatterns = [
     re_path(r'^$', homePage, name='home'),
+    re_path(r'^playerSearch.*$', searchView, name='playerSearch'),
     path('admin/', admin.site.urls)
 ]
